ce98b064582173343aea5ff9a2e092925257fe02
SonNguyen13 <sonnt13@vng.com.vn>
2023-10-19 12:22:24 +0700

    feat: add ToggleTerm

--- init.lua
+++ init.lua
@@ -67,6 +67,9 @@ vim.opt.rtp:prepend(lazypath)
 require('lazy').setup({
   -- NOTE: First, some plugins that don't require any configuration
 
+  -- ToggleTerm
+  {'akinsho/toggleterm.nvim', version = "*", config = true},
+
   -- Git related plugins
   'tpope/vim-fugitive',
   'tpope/vim-rhubarb',
